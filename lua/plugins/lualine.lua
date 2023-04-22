bae6a9c9df063e210ccb13b7ec29beef23e46336
Facundo Padula <padulafacundo@gmail.com>
2023-04-22 21:33:34 +0200

    Organize plugins in different files

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,18 @@
+-- Set lualine as statusline
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_c = { { 'filename', path =  1 } },
+      },
+    },
+  },
+}
