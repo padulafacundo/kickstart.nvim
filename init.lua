3c7a0a2e44e77ccca2dd613e0b24aaeba24bdb3a
Facundo Padula <padulafacundo@gmail.com>
2023-04-22 16:29:25 +0200

    Alias Git blame with GBlame

--- init.lua
+++ init.lua
@@ -287,6 +287,9 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+-- [[Configure Fugitive ]]
+vim.cmd('command! Gblame Git blame') -- alias for Git blame
+
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
