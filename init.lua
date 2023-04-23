aeec1280bf45e822c792ff3fcc7ffa759f7e983f
Facundo Padula <padulafacundo@gmail.com>
2023-04-23 02:28:24 +0200

    Move diagnostics config to its own file

--- init.lua
+++ init.lua
@@ -129,12 +129,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-
+require('config.diagnostics').setup()
 require('config.lsp').setup()
 
 -- nvim-cmp setup
