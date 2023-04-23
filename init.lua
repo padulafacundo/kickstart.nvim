d212b45839348ff8770540c08f5c357da57d8a83
Facundo Padula <padulafacundo@gmail.com>
2023-04-23 02:40:06 +0200

    Make search case sensitive

--- init.lua
+++ init.lua
@@ -50,10 +50,6 @@ vim.o.breakindent = true
 -- Save undo history
 vim.o.undofile = true
 
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
