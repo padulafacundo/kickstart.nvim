34180f15c20d40e32a5a5e49117307e7de17316d
Facundo Padula <padulafacundo@gmail.com>
2023-04-23 02:33:59 +0200

    Move nvim-cmp config to its own file

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,6 +3,9 @@ return {
   {
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    config = function()
+      require('config.nvim-cmp').setup()
+    end
   },
 
   -- Detect tabstop and shiftwidth automatically
