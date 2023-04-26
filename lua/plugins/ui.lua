1be1f039dcd00909d0bc10fb2d7d4908809549e6
Facundo Padula <padulafacundo@gmail.com>
2023-04-26 21:26:25 +0200

    Display relative file name even on inactive sections

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -18,6 +18,9 @@ return {
       sections = {
         lualine_c = { { 'filename', path =  1 } },
       },
+      inactive_sections = {
+        lualine_c = { { 'filename', path =  1 } },
+      },
     },
   },
 }
