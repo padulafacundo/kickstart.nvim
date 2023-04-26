ff2ccb6f5bdfd4e25a472adcb40a528b3271300f
Facundo Padula <padulafacundo@gmail.com>
2023-04-26 21:43:08 +0200

    Change theme

---
+++ lua/config/onedark.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+function M.setup()
+  require('onedark').setup { style = 'darker' }
+  require('onedark').load()
+end
+
+return M
--- lua/plugins/color-themes.lua
+++ lua/plugins/color-themes.lua
@@ -3,7 +3,7 @@ return {
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      require('config.onedark').setup()
     end,
   },
 }
