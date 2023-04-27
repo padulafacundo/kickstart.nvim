505f3af03d8a3c9fcf34b45ea4c041be079ac311
Facundo Padula <padulafacundo@gmail.com>
2023-04-27 22:35:49 +0200

    Enable icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,7 +10,7 @@ return {
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
@@ -23,4 +23,18 @@ return {
       },
     },
   },
+
+  -- To enable icons the terminal should use a font that supports theme
+  -- A possible configuration with iterm2 is to use a different font for
+  -- non-ASCII characters
+
+  -- Enable icons for file types, etc
+  {
+    'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("nvim-web-devicons").setup {
+        default = true
+      }
+    end
+  }
 }
