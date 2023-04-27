9faf3414d0ed97cbb23c0a5786dfa79e2c13b7d1
Facundo Padula <padulafacundo@gmail.com>
2023-04-27 21:17:34 +0200

    Move indent-blankline to editor plugin file

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -47,5 +47,17 @@ return {
   -- Auto-inserts end-blocks
   { 'tpope/vim-endwise' },
 
-  { 'tpope/vim-unimpaired' }
+  { 'tpope/vim-unimpaired' },
+
+  -- Add indentation guides even on blank lines
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+      show_current_context = true,
+    },
+  },
 }
