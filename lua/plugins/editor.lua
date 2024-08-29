5bcc33753d268b1a49ac90cb01c02f76f77549d4
Facundo Padula <padulafacundo@gmail.com>
2024-08-29 15:53:59 +0200

    Use indent-blankline v3

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -37,12 +37,9 @@ return {
   -- Add indentation guides even on blank lines
   {
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
+      indent = { char = '┊' },
     },
   },
 
@@ -59,16 +56,4 @@ return {
   },
 
   { 'tpope/vim-unimpaired' },
-
-  -- Add indentation guides even on blank lines
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
-    },
-  },
 }
